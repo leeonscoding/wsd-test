187ab78f7a2b07b4c29a48bd8d87dc10b5beff41
leeonscoding <leeonscoding@gmail.com>
2024-04-07 13:04:28 +0600

    Initial commit with 2 profiles

---
+++ src/main/java/com/leeonscoding/wsdtest/WsdTestApplication.java
@@ -0,0 +1,13 @@
+package com.leeonscoding.wsdtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsdTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsdTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leeonscoding/wsdtest/WsdTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leeonscoding.wsdtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsdTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
