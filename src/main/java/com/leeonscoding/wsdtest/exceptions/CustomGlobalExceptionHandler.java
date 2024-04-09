9d5597d8c62daa8c16375f1632125d6e285c14b4
leeonscoding <leeonscoding@gmail.com>
2024-04-09 11:36:24 +0600

    global exception handler

---
+++ src/main/java/com/leeonscoding/wsdtest/exceptions/CustomGlobalExceptionHandler.java
@@ -0,0 +1,58 @@
+package com.leeonscoding.wsdtest.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.leeonscoding.wsdtest.payloads.APIResponse;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.validation.ConstraintViolationException;
+
+@RestControllerAdvice
+public class CustomGlobalExceptionHandler {
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<APIResponse> myAPIException(APIException e) {
+        String message = e.getMessage();
+
+        APIResponse res = new APIResponse(message);
+
+        return new ResponseEntity<APIResponse>(res, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Map<String, String>> myMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        Map<String, String> res = new HashMap<>();
+
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError) err).getField();
+            String message = err.getDefaultMessage();
+
+            res.put(fieldName, message);
+        });
+
+        return new ResponseEntity<Map<String, String>>(res, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<Map<String, String>> myConstraintsVoilationException(ConstraintViolationException e) {
+        Map<String, String> res = new HashMap<>();
+
+        e.getConstraintViolations().forEach(voilation -> {
+            String fieldName = voilation.getPropertyPath().toString();
+            String message = voilation.getMessage();
+
+            res.put(fieldName, message);
+        });
+
+        return new ResponseEntity<Map<String, String>>(res, HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/main/java/com/leeonscoding/wsdtest/payloads/APIResponse.java
@@ -0,0 +1,4 @@
+package com.leeonscoding.wsdtest.payloads;
+
+public record APIResponse(String message) {
+}
